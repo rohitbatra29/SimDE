fd1c587dacd02db2768979353b576d2fcaf78db5
Rohit Batra <rbat@ucdavis.edu>
2025-12-05 13:55:58 -0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -94,29 +94,3 @@ SimDE_ui <- tagList(
              theme =  bs_theme(version = 5, bg = "white", fg = "#0d0c0c")
   )
 )
-
-# Things to do!
-# add a small description along with equation so that people who don't know the particular model can get a small jist
-# tool tips - pop ups for parameter names (https://educationshinyappteam.github.io/Style_Guide/commonElements.html#popovers-and-tooltips)
-# how can we add a random button beside set your seed, so that whenever someone clicks on it, it fills a random value in the seed input. Would be neat if this is possible!
-# require/validate
-# Maybe remove the word Undamped from Harmonic because it might not be undamped in stochastic systems.
-# the plots of some linear ones have the mu as a dashed line while some plots don't. Decide on the consistent look!
-# Will have to say that there might be a lag with the oscillatory SDE functions in the bivariate and it is better to write your number, instead of increasing/decreasing in the input, because then it tries to simulate each of those possibilities. You can also show this as a popup in the cases where Julia is used.
-
-# Other Stuff (not a priority)
-# maybe when you make the github repository public later, change the svg icon links to be from files in the github repository instead of the website.
-# check if you can progress indicator about how much time is left for the plot to be done so that the user knows to wait. Julia has a progress bar thing https://docs.sciml.ai/DiffEqDocs/stable/features/progress_bar/ but Idk how to integrate it into Shiny's progress bar https://shiny.posit.co/r/articles/build/progress/
-# add a run button so they don't run it agian and again?
-
-
-# Helpful tips
-# conditonal goes for every variable that changes the type of output panel you have
-# observe in server goes for every variable that changes the type of options you have
-# switch - function for calling functions?
-
-# always input "unevaluated" reactive values into a module server!!
-#? you can pass an unevaluated reactive value to the module and then evaluate it inside the module??
-# For example (refer to screenshot in old folder) but basically, put the reactive value (just the name) in the module server function and then within the function, put it as an evaluation with () paranethesis.
-# similarly you can do this with multiple reactive values if you have them in a reactiveValues object.
-# similarly you can return reactive values from a module serve using a return function, so that they can be used in the main app.
